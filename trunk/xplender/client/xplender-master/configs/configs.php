4ae2adb4c64bbae0ed054e92e959585f8eb6f9d4
chen <chen@6bd5eb31-355a-43c3-b832-d0f0fc470db9>
2016-04-04 14:07:33 +0000

    chen: modify root password

--- trunk/xplender/client/xplender-master/configs/configs.php
+++ trunk/xplender/client/xplender-master/configs/configs.php
@@ -2,7 +2,7 @@
   
   define("DB_HOST","localhost");
   define("DB_USER","root");
-  define("DB_PWD","");
+  define("DB_PWD","abcd1234");
   define("DB_DBNAME","xplender");
   define("DB_CHARSET","utf-8");
 
